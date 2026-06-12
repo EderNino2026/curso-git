64111cfbb9d1b36e9b3dbb3c556feea257e8dee5
EderNino2026 <edermora.0920@gmail.com>
2026-06-11 21:15:27 -0500

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,17 @@
+fh = open('mbox-short.txt')
+counts = dict()
+bigcount = None
+bigemail = None
+for lista in fh :
+    # lista = lista.strip()
+    datos = lista.split()
+    # print (datos)
+    if len(datos)< 3 or datos[0] !='From':
+        continue
+    email = datos[1]
+    counts[email] = counts.get(email,0) + 1
+for email,count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigemail = email
+        bigcount = count
+print(bigemail,bigcount)
